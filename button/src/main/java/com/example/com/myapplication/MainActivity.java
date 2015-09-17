55ef7ccb35587606582c36e51c286a89046dae17
SimYoungBo <sim4858@naver.com>
2015-09-17 20:21:15 +0900

    버튼 실습 완성품 첫번째 커밋

--- button/src/main/java/com/example/com/myapplication/MainActivity.java
+++ button/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.com.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.button1);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "버튼이 눌렸나요", Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    public void onClick(View view){
+       // Button button = (Button)findViewById(view.getId());
+        switch (view.getId()){
+            case R.id.button0: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("www.nate.com")));
+                break;
+            case R.id.button1: startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911")));
+                break;
+            case R.id.button2: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/external/images/media")));
+                break;
+            case R.id.button3: finish();
+                break;
+        }
+
     }
 
 }
