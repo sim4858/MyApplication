d3494a2b4f295bbeed9be5ef9b16cdab87f9cc7c
SimYoungBo <sim4858@naver.com>
2015-09-17 23:51:44 +0900

    버튼 실습 최종

--- button/src/main/java/com/example/com/myapplication/MainActivity.java
+++ button/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.com.myapplication;
 
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -18,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view){
        // Button button = (Button)findViewById(view.getId());
         switch (view.getId()){
-            case R.id.button0: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("www.nate.com")));
+            case R.id.button0: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nate.com")));
                 break;
             case R.id.button1: startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911")));
                 break;
-            case R.id.button2: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/external/images/media")));
+            case R.id.button2: startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media")));
                 break;
             case R.id.button3: finish();
                 break;
