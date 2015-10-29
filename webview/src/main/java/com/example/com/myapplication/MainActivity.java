a96cecfa9db8cd1ab3ad73d30c53dc207d53243c
SimYoungBo <sim4858@nvaer.com>
2015-10-29 11:24:16 +0900

    WebView clear

---
+++ webview/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+        web.setWebViewClient(new PrinceYWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String urlTemp = edtUrl.getText().toString();
+                if (!urlTemp.matches("http://.*")) {
+                    urlTemp = "http://" + urlTemp;
+                    edtUrl.setText(urlTemp);
+                }
+                web.loadUrl(urlTemp);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.goBack();
+            }
+        });
+
+    }
+
+    class PrinceYWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+            edtUrl.setText(url);
+        }
+    }
+
+}
+
